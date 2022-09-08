96aa578fb3542f98eb2bb6a69b6579b0b7e0b3a2
Ali Qorbani <ghorbani2006@gmail.com>
2022-09-08 17:15:31 +0430

    added static page.php its content is provided by the interviewer https://noavarpub.com/interview/php/page.php

---
+++ page.php
@@ -0,0 +1,36 @@
+<body>
+
+<form action="">
+
+    First Form:
+    <br>
+
+    <input type="text" name="s">
+
+    <input type="submit" value="Search">
+
+</form>
+
+
+<p>
+        
+        <span id="hello_world_span">
+            Hello World
+        </span>
+
+</p>
+
+<form action="">
+
+    Second Form:
+    <br>
+
+    <input type="text" name="MyInput">
+
+    <input type="submit">
+
+
+</form>
+
+
+</body>
