9b2acc405543dc07d7bf5783a57a9550db23634c
Ali Qorbani <ghorbani2006@gmail.com>
2022-09-08 17:29:38 +0430

    javascript quiz done

---
+++ javascript-quiz.js
@@ -0,0 +1,6 @@
+let second_form = document.querySelectorAll('form')[1],//we get second form it counts from 0
+    hello_word_span = document.getElementById("hello_world_span");
+
+hello_word_span.addEventListener("click", function () {
+    second_form.submit();
+});
